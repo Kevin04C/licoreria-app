6534e9d739e0a983a2314ea89f5e77b3ff8d0247
Kevin04c <kevincespedes2004@gmail.com>
2023-07-03 23:54:02 -0500

    Add middlware for authentication

---
+++ src/middleware.js
@@ -0,0 +1,13 @@
+import { getToken } from 'next-auth/jwt'
+import { NextResponse } from 'next/server'
+
+export default async function middleware (req) {
+  if (req.nextUrl.pathname.startsWith('/dashboard')) {
+    const session = await getToken({ req })
+    if (!session) {
+      return NextResponse.redirect(new URL('/', req.url))
+    }
+  }
+
+  return NextResponse.next()
+}
