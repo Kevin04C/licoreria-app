ab35541723b444b52f6b9411ced5d25c7057271c
Kevin04c <kevincespedes2004@gmail.com>
2023-07-04 00:22:01 -0500

    Fix error redirect when user is not authenticated

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 
 export default async function middleware (req) {
-  if (req.nextUrl.pathname.startsWith('/dashboard')) {
+  if (req.nextUrl.pathname.startsWith('/app')) {
     const session = await getToken({ req })
     if (!session) {
       return NextResponse.redirect(new URL('/', req.url))
